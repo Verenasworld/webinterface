da9c057fa37b57c8198ada120cb086cf83219cad
Verenasworld <verena.felizidas@gmail.com>
2021-05-18 15:06:50 +0200

    componentd DidMount setState

--- src/components/App.js
+++ src/components/App.js
@@ -13,6 +13,20 @@ constructor(){
   }
 }
 
+componentDidMount(){
+  fetch('./data.json')
+  .then(response => response.json())
+  .then(result => {
+    const apts = result.map(item => {
+      return item;
+    })
+
+    this.setState({
+      myAppointments: apts
+    })
+  })
+}
+
   render(){
     return (
       <main className="page bg-white" id="petratings">
