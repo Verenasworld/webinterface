ca91d1ec6fc3d56371b6169240dd55e16ffeab15
Verenasworld <verena.felizidas@gmail.com>
2021-05-18 15:22:37 +0200

    use data

---
+++ src/components/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import '../css/App.css';
+import AddApointsment from './AddApointsment';
+import ListApoointsment from './ListApoointsment';
+import SearchApointments from './SearchApointments';
+
+
+export default class App extends Component {
+constructor(){
+  super();
+  this.state={
+    myAppointments:[]
+  }
+}
+
+// lebenszyklua methode wird ausgeführt nachdem das Dom gerendert wurde !! ( externe daten einfügen)
+componentDidMount(){
+  fetch('./data.json')
+  //promiss 
+  .then(response => response.json())
+  .then(result => {
+    const apts = result.map(item => {
+      return item;
+    })
+    //status ändern
+    this.setState({
+      myAppointments: apts
+    })
+  })
+}
+
+  render(){
+    const listItems = this.state.myAppointments.map(item => (
+      <div>{item.petName}</div>
+    ));
+    return (
+      <main className="page bg-white" id="petratings">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 bg-white">
+                <div className="container">
+                  {listItems}
+                  <AddApointsment></AddApointsment>
+                   <ListApoointsment></ListApoointsment>
+                    <SearchApointments></SearchApointments>
+                 
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
+    );
+  }
+  
+  }
+  
+
+
+
