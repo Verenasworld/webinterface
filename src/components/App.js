58a19e6b443911d104577cf9d919335bbac22962
Verenasworld <verena.felizidas@gmail.com>
2021-05-20 09:16:26 +0200

    use lodash without, and bind this

--- src/components/App.js
+++ src/components/App.js
@@ -48,7 +48,7 @@ componentDidMount(){
     return (
       <main className="page bg-white" id="petratings" >
           <div className="container" >
-            <div className="row" id="test">
+            <div className="row" >
               <div className="col-md-12 bg-white">
                 <div className="container" >
                   <AddApointsment />
